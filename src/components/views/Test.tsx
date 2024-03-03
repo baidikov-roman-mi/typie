58c06d20e33d24df8cb91e1f6a1a6ec6a7871e89
Baidikov Roman <103574775+baidikov-roman-mi@users.noreply.github.com>
2024-03-03 14:30:48 +0200

    update: random text in textarea's placeholder

--- src/components/views/Test.tsx
+++ src/components/views/Test.tsx
@@ -1,6 +1,32 @@
 import "./_test.scss"
 
 const Test = () => {
+  const words = [
+    "lorem",
+    "ipsum",
+    "dolor",
+    "sit",
+    "amet",
+    "consectetur",
+    "adipiscing",
+    "elit",
+    "sed",
+    "do",
+    "eiusmod",
+    "tempor",
+    "incididunt",
+    "ut",
+    "labore",
+    "et",
+    "dolore",
+    "magna",
+    "aliqua"
+  ]
+  let text = ""
+  for (let i = 0; i < words.length; i++) {
+    const index = Math.floor(Math.random() * words.length)
+    text += words[index] + " "
+  }
   function handlePrevent(event: React.ClipboardEvent<HTMLTextAreaElement>) {
     event.preventDefault()
   }
@@ -11,8 +37,9 @@ const Test = () => {
         className="textarea"
         name="test"
         id="test-textarea"
-        placeholder="34567890-"
+        placeholder={text}
         spellCheck={false}
+        autoFocus
         onCut={handlePrevent}
         onCopy={handlePrevent}
         onPaste={handlePrevent}
